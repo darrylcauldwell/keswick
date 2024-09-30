70b8f0fe4302a39a641e40e09bba119ace278451
Darryl Cauldwell <darrylcauldwell@users.noreply.github.com>
2024-09-30 13:21:44 +0100

    Changed model from haarcascade_frontalface_default.xml to haarcascade_eye.xml

--- face-detector/app.py
+++ face-detector/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 def detect_faces(frame):
     # Load the pre-trained Haar Cascade face detection classifier
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
     # Convert the frame to grayscale for face detection
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
